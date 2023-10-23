c09a5d25fc4c4c86a0f3cbb9be20ce2cb601475c
dnlv <dnlv.andr@gmail.com>
2023-10-23 20:42:22 +0300

    4.0.1 - Mobile menu fixes

--- assets/scripts.js
+++ assets/scripts.js
@@ -13,35 +13,9 @@ function initCardAnimations() {
 
 initCardAnimations();
 
-let touchStartY = 0;
-let touchEndY = 0;
 
-document.addEventListener('touchstart', function(event) {
-    touchStartY = event.touches[0].clientY;
-}, false);
 
-document.addEventListener('touchmove', function(event) {
-    touchEndY = event.touches[0].clientY;
-}, false);
-
-document.addEventListener('touchend', function(event) {
-    // Detect swipe down at the top of the page
-    if (window.scrollY <= 0 && touchEndY > touchStartY + 50) { // 50px threshold for swipe
-        toggleMenu();
-    }
-}, false);
-
-function toggleMenu() {
-    const header = document.querySelector('.header');
-    const navToggle = document.querySelector('.nav-toggle');
-    const mainContainer = document.querySelector('.main-container');
-
-    header.classList.toggle('opened');
-    navToggle.classList.toggle('cross');
-    mainContainer.classList.toggle('slided');
-}
-
-// Existing code for toggling top bar
+// Code for toggling top bar
 function toggleTopBar() {
     const header = document.querySelector('.header');
 
@@ -52,14 +26,70 @@ function toggleTopBar() {
     }
 }
 
-// Existing code for collapsing when clicked outside
+// Menu
+// ==================================================================
+
+let touchStartY = 0;
+let touchEndY = 0;
+
+const mainContainer = document.querySelector('.main-container');
+
+mainContainer.addEventListener('touchstart', function(event) {
+    touchStartY = event.touches[0].clientY;
+}, false);
+
+mainContainer.addEventListener('touchmove', function(event) {
+    touchEndY = event.touches[0].clientY;
+}, false);
+
+mainContainer.addEventListener('touchend', function(event) {
+    // Detect swipe down at the top of the page
+    if (window.scrollY <= 10 && touchEndY > touchStartY + 50) { // 50px threshold for swipe
+        toggleMenu();
+    }
+}, false);
+
+function toggleMenu() {
+    const header = document.querySelector('.header');
+    const navToggle = document.querySelector('.nav-toggle');
+    const mainContainer = document.querySelector('.main-container');
+    const navItems = Array.from(document.querySelectorAll('.header .anim-nav')).reverse();
+
+    header.classList.toggle('opened');
+    navToggle.classList.toggle('cross');
+    mainContainer.classList.toggle('slided');
+
+    if (header.classList.contains('opened')) {
+        navItems.forEach((item, index) => {
+            setTimeout(() => {
+                item.style.opacity = "1";
+                item.style.transform = "translate3d(0, 0, 0)";
+            }, index * 80); // 100ms delay between items
+        });
+    } else {
+        navItems.forEach(item => {
+            item.style.opacity = "";
+            item.style.transform = "";
+        });
+    }
+}
+
+// Code for collapsing when clicked outside
 document.addEventListener('click', function(event) {
     const header = document.querySelector('.header');
+    const navToggle = document.querySelector('.nav-toggle');
+    const mainContainer = document.querySelector('.main-container');
 
-    if (!header.contains(event.target)) {
+    if (!header.contains(event.target) && event.target !== navToggle) {
         header.classList.remove('opened');
-        // Add more class removals if needed
+        navToggle.classList.remove('cross');
+        mainContainer.classList.remove('slided');
     }
+});
+
+// Listener for the nav toggle button
+document.querySelector('.nav-toggle').addEventListener('click', function() {
+    toggleMenu();
 });
 
 // Call toggleTopBar when the page is scrolled
